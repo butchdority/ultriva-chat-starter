2398d0fb7e0cf4645a7890f5fbe2926599255515
Butch Dority <butch@belsis.es>
2025-09-12 14:12:44 -0400

    chore: add SSE heartbeat keepalive

--- lib/stream.ts
+++ lib/stream.ts
@@ -1,16 +1,27 @@
 export function getEventStream() {
-  const encoder = new TextEncoder()
-  let controller: ReadableStreamDefaultController<any>
+  const encoder = new TextEncoder();
+  let controller: ReadableStreamDefaultController<any>;
 
   const readable = new ReadableStream({
-    start(c) { controller = c },
+    start(c) {
+      controller = c;
+
+      // send heartbeat every 20s to keep connection alive
+      setInterval(() => {
+        controller.enqueue(encoder.encode(": keepalive\n\n"));
+      }, 20000);
+    },
     cancel() {}
-  })
+  });
 
   function send(obj: any) {
-    const data = `data: ${JSON.stringify(obj)}\n\n`
-    controller.enqueue(encoder.encode(data))
+    const data = `data: ${JSON.stringify(obj)}\n\n`;
+    controller.enqueue(encoder.encode(data));
   }
-  function close() { controller.close() }
-  return { readable, push: send, close }
+
+  function close() {
+    controller.close();
+  }
+
+  return { readable, push: send, close };
 }
